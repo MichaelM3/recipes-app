907fde401cd4106237ea29a7d2aaabad68dcfe3c
michaelm3 <michael.muniz94@gmail.com>
2022-06-03 14:14:22 -0400

    added opacity hover and link to recipe items

--- src/components/RecipeItem.tsx
+++ src/components/RecipeItem.tsx
@@ -1,11 +1,15 @@
 import { FC } from "react";
+import { Link } from "react-router-dom";
 import { IRecipeItemProps } from "../Interfaces";
 
-const RecipeItem: FC<IRecipeItemProps> = ({ recipe: { title, image } }) => {
+const RecipeItem: FC<IRecipeItemProps> = ({ recipe: { id, title, image } }) => {
   return (
-    <div className="flex flex-col flex-wrap w-[250px] items-center text-center border-[1px] boreder-black p-3">
+    <div className="group flex flex-col flex-wrap justify-between w-[250px] items-center text-center border-[1px] p-3 hover:bg-opacity-90 hover:cursor-pointer">
       <h3>{title}</h3> 
-      <img src={image} alt="Food" className="object-cover w-[200px] h-[200px] rounded-full" />
+      <Link to={`recipes/${id}`} className="relative top-28 invisible group-hover:visible font-bold text-lg">
+        View Recipe
+      </Link>
+      <img src={image} alt="Food" className="group-hover:opacity-20 object-cover w-[200px] h-[200px] rounded-full" />
     </div>
   )
 
