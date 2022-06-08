0fa684c5cf4dff44454cb3287003caa2dc2172d4
MichaelM3 <michael.muniz94@gmail.com>
2022-06-07 22:10:26 -0400

    added ingredients to the page

--- src/components/RecipeProfile.tsx
+++ src/components/RecipeProfile.tsx
@@ -10,19 +10,34 @@ const RecipeProfile: FC = () => {
 
   const fetchRecipe = async (id: number) => {
     const res = await fetch(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${API_KEY}&includeNutrition=false`)
-    const { title, image, summary } = await res.json()
-    setRecipe({ id, title, image, summary })
+    const { title, image, summary, extendedIngredients } = await res.json()
+    const ingredients = extendedIngredients.map((ingredient: any) => ingredient.original) // Only pulling recipe instruction from ingredient object
+    setRecipe({ id, title, image, summary, ingredients })
   }
   
   useEffect(() => {
     fetchRecipe(location.state as number)
   }, [])
 
+  const displayIngredients = recipe?.ingredients.map((ingredient: string, index: number) => <li className="text-white" key={index}>{ingredient}</li>)
+
+  console.log(recipe);
+  
   return (
-    <div className="flex flex-col items-center space-y-5">
-      <h1 className="text-white text-3xl font-bold">{recipe?.title}</h1>
+    <div className="flex items-start justify-around h-full mt-9">
+      <div className="w-1/4">
+        <h1 className="text-white text-3xl font-bold">{recipe?.title}</h1>
+        <hr />
+        <p className="text-white">{recipe?.summary.replace( /(<([^>]+)>)/ig, "")}</p>
+      </div>
       <img src={recipe?.image} alt="Food" />
-      <div className="text-white w-1/2">{recipe?.summary.replace( /(<([^>]+)>)/ig, "")}</div>
+      <div>
+        <h2 className="text-white text-3xl">Ingredients</h2>
+        <ul className="list-disc space-y-4">
+          <hr />
+          {displayIngredients}
+        </ul>
+      </div>
     </div>
   )
 }
