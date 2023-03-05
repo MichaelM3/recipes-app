6e41806d8905d96d8da6e597e64c9c87a2a8c8eb
MichaelM3 <michael.muniz94@gmail.com>
2023-03-05 02:12:25 -0500

    almost finished register component

--- src/App.tsx
+++ src/App.tsx
@@ -8,29 +8,29 @@ import { IUser } from "./Interfaces/index"
 import { Routes, Route } from "react-router-dom"
 
 const App = () => {
-  const [user, setUser] = useState<IUser | null>(null)
+    const [user, setUser] = useState<IUser | null>(null)
 
-  useEffect(() => {
-    const savedUser = localStorage.getItem("user")
-    if (savedUser) {
-      setUser(JSON.parse(savedUser))
-    }
-  }, [])
+    useEffect(() => {
+        const savedUser = localStorage.getItem("user")
+        if (savedUser) {
+            setUser(JSON.parse(savedUser))
+        }
+    }, [])
 
-  console.log(user);
+    console.log(user);
 
-  return (
-    <main className="flex flex-col h-screen pt-3 bg-slate-600">
-      <Navbar user={user} setUser={setUser} />
-      {/* <hr className="border-1 border-black" /> */}
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/login" element={<Login setUser={setUser} />} />
-        <Route path="/register" element={<Register setUser={setUser} />}
-        <Route path="/recipes/:id" element={<RecipeProfile />} />
-      </Routes>
-    </main>
-  )
+    return (
+        <main className="flex flex-col h-screen pt-3 bg-slate-600">
+            <Navbar user={user} setUser={setUser} />
+            {/* <hr className="border-1 border-black" /> */}
+            <Routes>
+                <Route path="/" element={<Homepage />} />
+                <Route path="/login" element={<Login setUser={setUser} />} />
+                <Route path="/register" element={<Register setUser={setUser} />} />
+                <Route path="/recipes/:id" element={<RecipeProfile />} />
+            </Routes>
+        </main>
+    )
 }
 
 export default App
--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,12 +1,55 @@
-import { useState } from 'react'
+import { useState, FormEvent } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { IUser } from '../Interfaces';
 
 interface Props {
-    setUser: () => void;
+    setUser: (user: IUser) => void;
 }
 
-const Register = ({ setUser }:Props ) => {
+const Register = ({ setUser }: Props) => {
+
+    const [registerField, setRegisterField] = useState({
+        username: "",
+        password: "",
+        email: ""
+    })
+
+    const nav = useNavigate()
+
+    const handleRegisterSubmit = async (e: FormEvent) => {
+        e.preventDefault()
+        const res = await fetch(`http://localhost:8000/register`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                username: registerField.username,
+                email: registerField.email,
+                password: registerField.password
+            })
+        })
+        const data = await res.json()
+        if (data) {
+            setUser(data)
+            localStorage.setItem("user", JSON.stringify(data))
+            nav("/")
+        } else {
+            console.log("Invalid fields")
+        }
+    }
+
     return (
-        <div>Register</div>
+        <form onSubmit={handleRegisterSubmit} className="flex items-center justify-center h-2/3 p-3">
+            <div className="flex">
+                <span className="flex flex-col">
+                    <input type="text" placeholder="Username..." className="border-[1px] border-black rounded-tl-lg p-1" />
+                    <input type="text" placeholder="Email.." className="border-[1px] border-black border-t-0 rounded-bl-lg p-1" />
+                    <input type="password" placeholder="Password..." className="border-[1px] border-black border-t-0 rounded-bl-lg p-1" />
+                </span>
+                <button type="submit" className="border-[1px] border-black border-l-0 rounded-r-lg bg-slate-400 p-2">
+                    Login
+                </button>
+            </div>
+        </form>
     )
 }
 
